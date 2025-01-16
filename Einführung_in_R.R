5aedd6c52fc0c36a034f9f8a665b1e44b8790a9a
XaverHeuser <xaver.heuser@gmail.com>
2025-01-16 14:56:45 +0100

    First try

---
+++ Einführung_in_R.R
@@ -0,0 +1,31 @@
+#1
+x <- seq(0, 100, 5)
+print(x)
+
+#2
+vector_mean <- mean(c(5,6,7,4,3,2))
+print(vector_mean)
+
+#3
+
+
+#4
+
+
+#5
+
+
+#6
+vektor <- rep(3, times=100)
+print(vektor)
+
+#7
+z <- rep(sample(c(1,2), 1), 100)
+print(z)
+
+#8
+
+
+#9
+
+
