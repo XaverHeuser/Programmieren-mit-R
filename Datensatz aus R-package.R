ac726a76dedc6ba679cf251babcfa012a7b342bc
XaverHeuser <xaver.heuser@gmail.com>
2025-01-16 15:49:16 +0100

    First commands for datasets

---
+++ Datensatz aus R-package.R
@@ -0,0 +1,11 @@
+library(gapminder)
+library(forcats)
+library(dplyr)
+
+gp <- gapminder
+fct_count(gp$continent)
+gp$continent <- fct_expand(gp$continent, "Antarctica")
+fct_count(gp$continent)
+
+gp$continent <- fct_drop(gp$continent, "Antarctica")
+fct_count(gp$continent)
