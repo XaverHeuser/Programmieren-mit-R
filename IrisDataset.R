d39da0f454255d6e30e95d012d51c6b1066c06f2
XaverHeuser <xaver.heuser@gmail.com>
2025-01-17 10:48:43 +0100

    Finish task Iris Dataset

--- IrisDataset.R
+++ IrisDataset.R
@@ -24,17 +24,61 @@ modus(data$Sepal.Length)
 ggplot(data, aes(x = Sepal.Length)) +
 geom_histogram(binwidth = 0.2, fill = "red", color ="black") +
 labs( title = "Titel des Histogramms",
-x = "Beschriftung der X-Achse",
-y = "Beschriftung der Y-Achse" )
+x = "Sepal Length",
+y = "Values" )
 # TODO: Interpretation:
 
 #7 Create boxplot ----
 ggplot(data, aes(x = Species, y = Sepal.Width)) +
 geom_boxplot(fill = "green", color = "white") +
 labs( title = "Titel des Boxplots",
-x = "BeschriIung der X-Achse",
-y = "BeschriIung der Y-Achse" )
+x = "Species",
+y = "Sepal Width" )
 # TODO: Interpretation:
 
 #8 Group and Aggregate ----
 aggregate(data$Sepal.Length, by = list(data$Species), FUN = mean)
+
+#9 Create new variable ----
+data$Sepal.Size <- ifelse(data$Sepal.Length > 5.0, "Ja", "Nein")
+
+#10 Save dataset ----
+write.csv(data, file = "./data/iris_dataset_edited.csv")
+
+#11 Filter data ----
+data_filtered <- data[data$Sepal.Length > 6.0, ]
+
+#12 Calculate standard deviation ----
+sd(data$Petal.Length)
+
+#13 Calculate interquartile range ----
+IQR(data$Sepal.Width)
+
+#14 Create scatterplot ----
+ggplot(data, aes(x = Sepal.Width, y = Petal.Width)) +
+geom_point(aes(color = Species)) +
+labs( title = "Scatterplot",
+x = "Sepal Width",
+y = "Petal Width" )
+
+#15 Rename column ----
+colnames(data)[1] <- "SepalLength_cm"
+head(data)
+
+#16 Maximum and minimum ----
+max(data$Petal.Length)
+min(data$Petal.Length)
+
+#17 Count values per Species ----
+fct_count(data$Species)
+
+#18 Sort data ----
+data_sorted <- data[order(data$SepalLength_cm), ]
+head(data_sorted)
+
+#19 Get rows with missing values ----
+sum(is.na(data))
+
+#20 Mean of columns for each Species ----
+aggregate(data[, 1:4], by = list(data$Species), FUN = mean)
+aggregate(data$Petal.Width, by = list(data$Species), FUN = mean)
